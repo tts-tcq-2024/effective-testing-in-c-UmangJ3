91fcbd380d7933089c4d3f5e91ed964b92af65c0
UmangJ3 <haharofl14@gmail.com>
2024-08-16 12:27:36 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -8,8 +8,8 @@ int printColorMap(char* buffer) {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-	    printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
-            sprintf(buffer + strlen(buffer), "%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);  // Bug: minorColor[i] should be minorColor[j]
+            sprintf(buffer + strlen(buffer), "%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]); // Bug: same here
         }
     }
     return i * j;
@@ -18,35 +18,22 @@ int printColorMap(char* buffer) {
 int main() {
     char buffer[1000] = {0};
     int result = printColorMap(buffer);
-    const char* expectedResults =
-        "0 | White | Blue\n"
-        "1 | White | Orange\n"
-        "2 | White | Green\n"
-        "3 | White | Brown\n"
-        "4 | White | Slate\n"
-        "5 | Red | Blue\n"
-        "6 | Red | Orange\n"
-        "7 | Red | Green\n"
-        "8 | Red | Brown\n"
-        "9 | Red | Slate\n"
-        "10 | Black | Blue\n"
-        "11 | Black | Orange\n"
-        "12 | Black | Green\n"
-        "13 | Black | Brown\n"
-        "14 | Black | Slate\n"
-        "15 | Yellow | Blue\n"
-        "16 | Yellow | Orange\n"
-        "17 | Yellow | Green\n"
-        "18 | Yellow | Brown\n"
-        "19 | Yellow | Slate\n"
-        "20 | Violet | Blue\n"
-        "21 | Violet | Orange\n"
-        "22 | Violet | Green\n"
-        "23 | Violet | Brown\n"
-        "24 | Violet | Slate\n";
+    
+    const char* expectedMajorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* expectedMinorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    
+    char line[100];
+    int index = 0;
 
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            sprintf(line, "%d | %s | %s\n", index, expectedMajorColors[i], expectedMinorColors[j]);
+            assert(strncmp(buffer + strlen(buffer) - strlen(line), line, strlen(line)) == 0); 
+            index++;
+        }
+    }
+    
     assert(result == 25);
-    assert(strcmp(buffer, expectedResults) == 0);  
     printf("All is well (maybe!)\n");
     return 0;
 }
