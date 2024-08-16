fb734d2ae129838f032b8ce4f6cfeccf4c1ce05f
UmangJ3 <haharofl14@gmail.com>
2024-08-16 12:16:03 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -16,13 +16,37 @@ int printColorMap() {
 }
 
 int main() {
-    int result = printColorMap();
+    char buffer[1000] = {0};
+    int result = printColorMap(buffer);
+    const char* expectedResult =
+        "0 | White | Blue\n"
+        "1 | White | Orange\n"
+        "2 | White | Green\n"
+        "3 | White | Brown\n"
+        "4 | White | Slate\n"
+        "5 | Red | Blue\n"
+        "6 | Red | Orange\n"
+        "7 | Red | Green\n"
+        "8 | Red | Brown\n"
+        "9 | Red | Slate\n"
+        "10 | Black | Blue\n"
+        "11 | Black | Orange\n"
+        "12 | Black | Green\n"
+        "13 | Black | Brown\n"
+        "14 | Black | Slate\n"
+        "15 | Yellow | Blue\n"
+        "16 | Yellow | Orange\n"
+        "17 | Yellow | Green\n"
+        "18 | Yellow | Brown\n"
+        "19 | Yellow | Slate\n"
+        "20 | Violet | Blue\n"
+        "21 | Violet | Orange\n"
+        "22 | Violet | Green\n"
+        "23 | Violet | Brown\n"
+        "24 | Violet | Slate\n";
+
     assert(result == 25);
-    
-    // Checking the first and last combination to ensure alignment
-    assert(strcmp("White", "White") == 0 && strcmp("Blue", "Blue") == 0); 
-    assert(strcmp("Violet", "Violet") == 0 && strcmp("Slate", "Slate") == 0);
-    
+    assert(strcmp(buffer, expectedResult) == 0); 
     printf("All is well (maybe!)\n");
     return 0;
 }
